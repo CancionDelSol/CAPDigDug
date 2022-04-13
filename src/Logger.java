fe007c7d6c6e746b44c04e2f8e5acb730a91bcdc
Roger Johnson <rkj8@students.uwf.edu>
2022-04-13 13:17:22 +0000

    Matrix Functional

--- src/Logger.java
+++ src/Logger.java
@@ -1,27 +1,33 @@
+import java.util.Date;
+
 public class Logger {
+    //region Fields
+    private LogLevel _logLevel = LogLevel.DEBUG;
+    //endregion
+
     //region Logger
-    public void Debug(String msg) {
+    public static void Debug(String msg) {
         Write(LogLevel.DEBUG, msg);
     }
-    public void Verbose(String msg) {
+    public static void Verbose(String msg) {
         Write(LogLevel.VERBOSE, msg);
     }
-    public void Warn(String msg) {
+    public static void Warn(String msg) {
         Write(LogLevel.WARN, msg);
     }
-    public void Error(String msg) {
+    public static void Error(String msg) {
         Write(LogLevel.ERROR, msg);
     }
-    public void Gui(String msg) {
+    public static void Gui(String msg) {
         Write(LogLevel.GUI, msg);
     }
     //endregion
 
     //region Private
-    private String GetNow() {
+    private static String GetNow() {
         return new Date().toString();
     }
-    private void Write(LogLevel level, String msg) {
+    private static void Write(LogLevel level, String msg) {
         System.out.println(
             String.format(
                 "%s | [%8s] : %s",
